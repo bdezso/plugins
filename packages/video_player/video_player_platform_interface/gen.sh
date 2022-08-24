623874569c1340b2522e8b4f9c22bd7f239c1ed7
Bence Dezso <dezsobence98@gmail.com>
2022-08-24 21:56:31 +0200

    ah

--- packages/video_player/video_player_platform_interface/gen.sh
+++ packages/video_player/video_player_platform_interface/gen.sh
@@ -1,1 +1,2 @@
 flutter pub run pigeon --input ./pigeons/messages.dart --objc_header_out=./auto-gen-pigeon-for-native-code/messages.g.h --objc_source_out=./auto-gen-pigeon-for-native-code/messages.g.m
+#lol
